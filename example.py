38251743221b0b85fb506b2af73ccf0440ef11ad
Alsrec <47378034+Alsrec@users.noreply.github.com>
2021-01-28 10:26:02 +0800

    Update example.py

--- example.py
+++ example.py
@@ -2,3 +2,11 @@
 
 def add3(a, b, c):
   returrn a + b + c
+  
+  
+#test function here
+
+def test_add3():
+  assert add3(1, 2, 3) == 6
+  assert add3(5, 5, 5,) == 15
+  assert add3("EA", "Z", "Y") == "EAZY"
