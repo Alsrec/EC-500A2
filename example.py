706af027dc11460af811391ddd515ba3917587af
Alsrec <47378034+Alsrec@users.noreply.github.com>
2021-01-28 11:33:38 +0800

    Update example.py

--- example.py
+++ example.py
@@ -11,10 +11,10 @@ def numpyaround(a, b):
   
 #test function here
 
-def test_add3():
-  assert add3(1, 2, 3) == 6
-  assert add3(5, 5, 5,) == 15
-  assert add3("EA", "Z", "Y") == "EAZY"
-
-def test_numpyaround():
-  assert numpyaround(1.222, 1) == 1.2
+#def test_add3():
+# assert add3(1, 2, 3) == 6
+#  assert add3(5, 5, 5,) == 15
+# assert add3("EA", "Z", "Y") == "EAZY"
+#
+#def test_numpyaround():
+#  assert numpyaround(1.222, 1) == 1.2
