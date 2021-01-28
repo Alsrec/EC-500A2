bcbde94393504621650346e0f66210a3797cafac
Alsrec <47378034+Alsrec@users.noreply.github.com>
2021-01-28 10:58:45 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,12 @@
 # example.py
+import numpy as np
 
 def add3(a, b, c):
   return a + b + c
+
+def numpyaround(a, b):
+  returen np.around(a, b)
+  
   
   
 #test function here
@@ -10,3 +15,6 @@ def test_add3():
   assert add3(1, 2, 3) == 6
   assert add3(5, 5, 5,) == 15
   assert add3("EA", "Z", "Y") == "EAZY"
+
+  def test_numpyaround:
+    assert numpyaround(1.222, 1) == 1.2
